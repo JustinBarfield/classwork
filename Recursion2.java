9f2189cb16a35cc1d37e7eb9768a3e1e47080d6e
JustinBarfield <122563454+JustinBarfield@users.noreply.github.com>
2023-01-24 19:41:40 +0000

    added binary search

--- Recursion2.java
+++ Recursion2.java
@@ -1,6 +1,27 @@
 public class Recursion2 {
 
-    private static int[] intArray = { 2, 7, 3, 8, 10 };
+    private static int[] intArray = { 2, 7, 3, 20, 8, 10 };
+    private static int[] sorted = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22 };
+
+    public static int binarySearch(int key, int left, int right) {
+        if (right == left) {
+            return sorted[left];
+        } else {
+            int middle = (left + right) / 2;
+            if (sorted[middle] == key)
+                return middle;
+            else if (sorted[middle] < key) {
+                left = middle+1;
+               
+            }
+            else //(sorted[middle] > key)
+            {
+                right = middle-1;
+            }
+            return binarySearch(key, left, right);
+
+        }
+    }
 
     /**
      * recursively adds up numbers
@@ -17,19 +38,31 @@ public class Recursion2 {
         }
     }
 
-    public static int sumByTail(int index, int sum) {
+    public static int maxByHead(int index) {
         if (index == intArray.length - 1) {
-            return sum + intArray[index];
+            return intArray[index];
         } else {
-            sum = sum + intArray[index];
-            return sumByTail(index + 1, sum);
+            int previousMax = maxByHead(index + 1);
+            return Math.max(previousMax, intArray[index]);
+        }
+    }
+
+    public static int maxByTail(int index, int max) {
+        if (index == intArray.length - 1) {
+            return Math.max(max, intArray[index]);
+        } else {
+            max = Math.max(max, intArray[index]);
+            return maxByTail(index + 1, max);
         }
     }
 
     public static void main(String[] args) {
 
-        System.out.println(sumByHead(0));
-        System.out.println(sumByTail(0, 0));
+        // System.out.println(sumByHead(0));
 
+        // System.out.println(sumByTail(0, 0));
+       // System.out.println(maxByHead(0));
+        //System.out.println(maxByTail(0, intArray[0]));
+        System.out.println(binarySearch(16,0,sorted.length-1));
     }
 }
