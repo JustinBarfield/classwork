2975b8339f02017044cde07540aa1331a047530a
JustinBarfield <122563454+JustinBarfield@users.noreply.github.com>
2023-01-24 19:04:20 +0000

    added sum by tail method

--- Recursion2.java
+++ Recursion2.java
@@ -2,6 +2,12 @@ public class Recursion2 {
 
     private static int[] intArray = { 2, 7, 3, 8, 10 };
 
+    /**
+     * recursively adds up numbers
+     * 
+     * @param index must be an integer in the array
+     * @return an integer representing the sum of the array elements
+     */
     public static int sumByHead(int index) {
         if (index == intArray.length - 1) {
             return intArray[index];
@@ -11,9 +17,19 @@ public class Recursion2 {
         }
     }
 
+    public static int sumByTail(int index, int sum) {
+        if (index == intArray.length - 1) {
+            return sum + intArray[index];
+        } else {
+            sum = sum + intArray[index];
+            return sumByTail(index + 1, sum);
+        }
+    }
+
     public static void main(String[] args) {
 
         System.out.println(sumByHead(0));
+        System.out.println(sumByTail(0, 0));
 
     }
 }
