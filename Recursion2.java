5e28891ff78cb5b15e69e41ef8ed4e35a714a393
JustinBarfield <122563454+JustinBarfield@users.noreply.github.com>
2023-01-26 19:00:09 +0000

    adjusted binary search

--- Recursion2.java
+++ Recursion2.java
@@ -4,24 +4,34 @@ public class Recursion2 {
     private static int[] sorted = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22 };
 
     public static int binarySearch(int key, int left, int right) {
-        if (right == left) {
-            return left;
+        if (right < left) {
+            return -1;
         } else {
             int middle = (left + right) / 2;
             if (sorted[middle] == key)
                 return middle;
             else if (sorted[middle] < key) {
-                left = middle+1;
-               
-            }
-            else //(sorted[middle] > key)
+                left = middle + 1;
+
+            } else 
             {
-                right = middle-1;
+                right = middle - 1;
             }
             return binarySearch(key, left, right);
 
         }
     }
+
+    public static int fib(int n) {
+        if (n == 0)
+            return 1;
+        else if (n == 1)
+            return 1;
+        else
+            return fib(n - 2) + fib(n - 1);
+    }
+
+ }
 
     /**
      * recursively adds up numbers
@@ -61,8 +71,8 @@ public class Recursion2 {
         // System.out.println(sumByHead(0));
 
         // System.out.println(sumByTail(0, 0));
-       // System.out.println(maxByHead(0));
-        //System.out.println(maxByTail(0, intArray[0]));
-        System.out.println(binarySearch(16,0,sorted.length-1));
+        // System.out.println(maxByHead(0));
+        // System.out.println(maxByTail(0, intArray[0]));
+        System.out.println(binarySearch(17, 0, sorted.length - 1));
     }
 }
